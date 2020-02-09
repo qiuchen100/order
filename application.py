e1f203e94afe1fd7b68fa3da04c85df3a25cab16
qiuchen <qiuchen100@163.com>
2020-02-09 14:31:35 +0800

    加入前端页面模块

--- application.py
+++ application.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
+from common.libs.urlManager import UrlManager
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 import os
+from www import register_blueprint
 
 
 class Application(Flask):
     def __init__(self, import_name):
-        super(Application, self).__init__(import_name)
+        super(Application, self).__init__(
+            import_name, static_folder='web/static', template_folder='web/templates')
         self.config.from_pyfile('config/base_setting.py')
         if 'ops_config' in os.environ:
             self.config.from_pyfile(
@@ -18,3 +21,11 @@ class Application(Flask):
 db = SQLAlchemy()
 app = Application(__name__)
 manager = Manager(app)
+register_blueprint(app)
+
+
+'''
+函数模板
+'''
+app.add_template_global(UrlManager.buildStaticUrl, 'buildStaticUrl')
+app.add_template_global(UrlManager.buildUrl, 'buildUrl')
